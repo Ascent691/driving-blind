2f9f6eb9bec87f0318239ce1a892ad75c5abac6d
SeanNeal572 <sean.neal@chillisoft.co.za>
2025-08-29 15:34:08 +0200

    Created cooler scenarios

---
+++ DrivingBlind/Infrastructure/CoolerScenario.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure
+{
+    public record CoolerScenario(Cell[,] Cells, int Width, int Height);
+}
